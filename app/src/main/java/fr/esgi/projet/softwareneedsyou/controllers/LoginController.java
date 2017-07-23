47f07def9b69e7f1c51985ceda5eda4d4aea5888
half-shell <michel.maubert.pro@gmail.com>
2017-07-23 16:21:50 +0200

    Cos:Bind error message on login

--- app/src/main/java/fr/esgi/projet/softwareneedsyou/controllers/LoginController.java
+++ app/src/main/java/fr/esgi/projet/softwareneedsyou/controllers/LoginController.java
@@ -1,48 +1,58 @@
 package fr.esgi.projet.softwareneedsyou.controllers;
+
 import com.mashape.unirest.http.exceptions.UnirestException;
 import fr.esgi.projet.softwareneedsyou.callbacks.UserCallback;
 import fr.esgi.projet.softwareneedsyou.models.DataModel;
 import fr.esgi.projet.softwareneedsyou.webApi.WebApiRequest;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
 import org.json.JSONException;
 
 
 public class LoginController {
 
-    @FXML private TextField usernameTextField;
-    @FXML private PasswordField passwordPasswordField;
-    @FXML private FlowPane startFlowPane;
-    @FXML private AnchorPane startAnchorPane;
-    @FXML private Hyperlink inscriptionHyperLink;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordPasswordField;
+    @FXML
+    private Label errorLabel;
+    private StringProperty errorMessage;
     private DataModel model;
 
+    @FXML
+    public void initialize() {
+        errorMessage = new SimpleStringProperty();
+        errorMessage.addListener((observableValue, s, t1) -> errorLabel.setText(observableValue.getValue()));
+    }
+
     public void initModel(DataModel model) {
-        if(this.model != null) {
+        if (this.model != null) {
             throw new IllegalStateException("There can be only one model.");
         }
         this.model = model;
     }
 
     @FXML
-    public void handleOk()  {
+    public void handleOk() {
         String username = String.valueOf(usernameTextField.getCharacters());
         String password = String.valueOf(passwordPasswordField.getCharacters());
         WebApiRequest war = new WebApiRequest();
         try {
             war.login(new UserCallback(), model, username, password);
+            errorMessage.setValue("");
         } catch (JSONException | UnirestException e) {
-            System.out.println("Try again");
+            errorMessage.setValue("Try again");
         }
     }
 
     public void handleRedirection(ActionEvent actionEvent) {
-        String url = "http://www.google.com";
+        String url = "http://www.google.com"; //TODO: Change the url to the actual website
         String os = System.getProperty("os.name").toLowerCase();
         Runtime rt = Runtime.getRuntime();
 
@@ -56,8 +66,9 @@ public class LoginController {
                         "netscape", "opera", "links", "lynx"};
 
                 StringBuffer cmd = new StringBuffer();
-                for (int i = 0; i < browsers.length; i++)
+                for (int i = 0; i < browsers.length; i++) {
                     cmd.append((i == 0 ? "" : " || ") + browsers[i] + " \"" + url + "\" ");
+                }
 
                 rt.exec(new String[]{"sh", "-c", cmd.toString()});
             } else {
